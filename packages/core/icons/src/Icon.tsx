91614e5f168a08cece608d2b4875f99c690cfebe
Muhammad Hashim <muhammadhashimsardar@gmail.com>
2025-10-04 07:01:10 +0500

    Commit changes in packages/core/icons/src/Icon.tsx

--- packages/core/icons/src/Icon.tsx
+++ packages/core/icons/src/Icon.tsx
@@ -1,10 +1,11 @@
 /**
  * @deprecated Use the new modular structure instead
  * This file is kept for backward compatibility
- * Import from the main package: import { Icon } from '@triadui/icons-core'
+ * Import from the main package: import { Icon } from '@triadui/icons'
  */
 
 // Re-export from the new structure
 export { Icon } from './components/Icon';
 export type * from './types';
+// eslint-disable-next-line react-refresh/only-export-components
 export * from './constants';
