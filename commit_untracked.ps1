0ddcf5b9186cfca60c97024dbc5b3cff930c75de
Muhammad Hashim <muhammadhashimsardar@gmail.com>
2025-10-04 07:03:03 +0500

    Remove temporary commit script

--- commit_untracked.ps1
+++
@@ -1,5 +0,0 @@
-$untrackedFiles = git status --porcelain | Where-Object { $_ -match '^\?\?' } | ForEach-Object { $_.Substring(3) }
-foreach ($file in $untrackedFiles) {
-    git add $file
-    git commit -m "Add $file"
-}
