b0c90695175dbb4ce99acf478464580a7be5b1e0
Muhammad Hashim <muhammadhashimsardar@gmail.com>
2025-10-04 07:02:50 +0500

    Add packages/core/icons/src/index.d.ts

---
+++ packages/core/icons/src/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @triadui/icons - Core Icon Component Package
+ *
+ * Modular structure for Material Design 3 icons
+ */
+export { Icon } from './components/Icon';
+export type * from './types';
+export type { IconFill, IconWeight, IconGrade, IconOpticalSize } from './types';
+export * from './constants';
+export * from './utils/cn';
+//# sourceMappingURL=index.d.ts.map
